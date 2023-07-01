562aa9f73e27e880f51e279b9179ffd565730685
sofiaslvn <slivinskasofia@gmail.com>
2023-07-01 12:55:31 +0300

    Flask HW

--- flaskProject1/app.py
+++ flaskProject1/app.py
@@ -19,34 +19,7 @@ dictConfig({
 
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello_world():
-    app.logger.info('Handling / endpoint')
-    return (
-        '<h1>Hello World!</h1>'
-    )
-
-@app.route('/html')
-def html_endpoint():
-    app.logger.info('Handling /html endpoint')
-    return '''
-        <html>
-            <body>
-                <h1>This is an HTML endpoint</h1>
-            </body>
-        </html>
-    '''
-
-@app.route('/json')
-def json_endpoint():
-    app.logger.info('Handling /json endpoint')
-    return {
-        'message': 'This is a JSON endpoint',
-        'data': {
-            'name': 'Jane',
-            'age': 25
-        }
-    }
+from views import *
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ flaskProject1/views.py
@@ -0,0 +1,69 @@
+from app import app
+
+from flask import Flask, request, redirect, abort
+
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    names = ['Andrew', 'Olga', 'Katerina', 'Dmytro', 'Volodymyr']
+    return str(names)
+
+
+@app.route('/books', methods=['GET'])
+def get_books():
+    books = ['Book 1', 'Book 2', 'Book 3', 'Book 4', 'Book 5']
+    book_list = '<ul>'
+    for book in books:
+        book_list += '<li>' + book + '</li>'
+    book_list += '</ul>'
+    return book_list
+
+
+@app.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    if user_id % 2 == 0:
+        return f'Text with id: {user_id}'
+    else:
+        abort(404, 'Not Found')
+
+
+@app.route('/books/<string:title>', methods=['GET'])
+def get_book(title):
+    transformed_title = title.capitalize()
+    return transformed_title
+
+
+@app.route('/params', methods=['GET'])
+def get_params():
+    param_table = '<table>'
+    param_table += '<tr><th>Parameter</th><th>Value</th></tr>'
+    for key, value in request.args.items():
+        param_table += f'<tr><td>{key}</td><td>{value}</td></tr>'
+    param_table += '</table>'
+    return param_table
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        login_form = '''
+            <form method="POST" action="/login">
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="username" required><br>
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required><br>
+                <input type="submit" value="Submit">
+            </form>
+        '''
+        return login_form
+    elif request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if username and password:
+            return redirect('/users')
+        else:
+            abort(400, 'Username or password missing')
+
+
+if __name__ == '__main__':
+    app.run()
